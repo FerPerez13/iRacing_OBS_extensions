154136de3034d66a088e4dbd63da4ecb1af157dc
FerPerez13 <fer13vlc@gmail.com>
2024-04-23 01:16:21 +0200

    Add txt info and threading tasks

--- iracing_calls.py
+++ iracing_calls.py
@@ -3,6 +3,8 @@ import main
 import models.login as login
 import models.member_profile as m_profile
 
+tread_member_profile_update = False
+
 def try_login():
     print(f"Username: {login.username}, Password: {login.password}")
     idc = irDataClient(username=login.username, password=login.password)
@@ -10,10 +12,34 @@ def try_login():
     member_profile = idc.member_profile(cust_id=784776)
 
     member_info = member_profile.get('member_info')
-    name = member_info.get('display_name')
+    m_profile.name = member_info.get('display_name')
 
     licenses = member_info.get('licenses')
+
     m_profile.sportscar_sr = licenses[1].get('safety_rating')
-    m_profile.formula_sr = licenses[2].get('safety_rating')
     m_profile.sportscar = licenses[1].get('irating')
+
+    m_profile.formula_sr = licenses[2].get('safety_rating')
     m_profile.formula = licenses[2].get('irating')
+
+
+def update_member_profile():
+    with open("results/name.txt", 'w') as f:
+        f.write(f"{m_profile.name}")
+        f.close()
+
+    with open("results/sportscar_sr.txt", 'w') as f:
+        f.write(f"{m_profile.sportscar_sr}")
+        f.close()
+
+    with open("results/formula_sr.txt", 'w') as f:
+        f.write(f"{m_profile.formula_sr}")
+        f.close()
+
+    with open("results/sportscar.txt", 'w') as f:
+        f.write(f"{m_profile.sportscar}")
+        f.close()
+
+    with open("results/formula.txt", 'w') as f:
+        f.write(f"{m_profile.formula}")
+        f.close()
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import tkinter as tk
 import login
 import models.member_profile as m_profile
+import iracing_calls
+import threading
+
+class TimerThread(threading.Thread):
+    def __init__(self, interval):
+        threading.Thread.__init__(self)
+        self.interval = interval
+        self.daemon = True  # Ensure thread ends when main program ends
+        self.running = False
+
+    def run(self):
+        self.running = True
+        while self.running:
+            iracing_calls.update_member_profile()
+            threading.Event().wait(self.interval)
+
+    def stop(self):
+        self.running = False
+
 class Main_UI(tk.Frame):
     """Docstring."""
     def __init__(self, parent=None):
@@ -12,32 +31,30 @@ class Main_UI(tk.Frame):
         self.parent.title("Main Window")
         self.parent.resizable(False, False)
 
-        # Create a label
-        self.name = tk.Label(self.parent, text=m_profile.name)
-        self.name.pack()
+        # create label to display member profile
+        self.member_profile_label = tk.Label(self.parent, text="Member Profile")
+        self.member_profile_label.pack()
 
-        # Create a label
-        self.road_sr = tk.Label(self.parent, text="ROAD SR " + m_profile.sportscar_sr)
-        self.road_sr.pack()
+        # create button to start timer to update member profile
+        self.start_button = tk.Button(self.parent, text="Start Process", command=self.start_member_profile_update_timer)
+        self.start_button.pack()
 
-        # Create a label
-        self.oval_sr = tk.Label(self.parent, text="OVAL SR " + m_profile.formula_sr)
-        self.oval_sr.pack()
+        self.stop_button = tk.Button(self.parent, text="Stop Process", command=self.stop_member_profile_update_timer)
+        self.stop_button.pack()
 
-        # Create a label
-        self.road = tk.Label(self.parent, text="ROAD " + m_profile.sportscar)
-        self.road.pack()
+        # create button to call iracing_calls.update_member_profile()
+        self.update_button = tk.Button(self.parent, text="Update Member Profile", command=iracing_calls.update_member_profile)
+        self.update_button.pack()
 
-        # Create a label
-        self.oval = tk.Label(self.parent, text="OVAL " + m_profile.formula)
-        self.oval.pack()
+    def start_member_profile_update_timer(self):
+        self.timer_thread = TimerThread(0)
+        self.timer_thread.start()
 
-    def update_labels(self):
-        self.name.config(text=m_profile.name)
-        self.road_sr.config(text="ROAD SR " + m_profile.sportscar_sr)
-        self.oval_sr.config(text="OVAL SR " + m_profile.formula_sr)
-        self.road.config(text="ROAD " + m_profile.sportscar)
-        self.oval.config(text="OVAL " + m_profile.formula)
+
+    def stop_member_profile_update_timer(self):
+        if self.timer_thread:
+            self.timer_thread.stop()
+            self.timer_thread = None
 
 def show_main_window():
     root = tk.Tk()
